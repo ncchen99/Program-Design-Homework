10802c9460f087d9ca51c0cc09a601f184f08294
ncchen99 <ncchen99@gmail.com>
2022-01-09 21:33:32 +0800

    😍

--- week18/pA.c
+++ week18/pA.c
@@ -4,6 +4,6 @@ int main() {
     char k[10000] = {0}, p[200000] = {0};
     gets(k);
     gets(p);
-    for (int i = 0; i < strlen(p); i++)
-        printf("%c", k[i % strlen(k)] ^ p[i]);
+    for (int i = 0; i < strlen(p); i++) 
+        printf("%c", k[i % strlen(k)] ^ p[i]); 
 }
